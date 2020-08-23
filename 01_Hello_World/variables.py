d9b67f616fce73dbf10e4149a4de643b0a704d08
SidorelaSuli <sidorela.suli@gmail.com>
2020-08-23 14:59:55 +0200

    using variables

---
+++ 01_Hello_World/variables.py
@@ -0,0 +1,12 @@
+name = "Sidorela"
+print("Hello World! My name is " + name + ".")
+
+name = "Sidorela Suli"
+print("My name is " + name + ".")
+
+# new line
+print("Hello\nWorld")
+
+#using quotes
+print("Hello World\nMy Name is " + name + ".")
+print("hello World\n\"My name is " + name + "\".")
