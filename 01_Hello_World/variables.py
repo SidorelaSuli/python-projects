f25c04dfa49edbc0c57eeffc308e17c326c846d8
SidorelaSuli <sidorela.suli@gmail.com>
2020-08-24 15:58:24 +0200

    functions

--- 01_Hello_World/variables.py
+++ 01_Hello_World/variables.py
@@ -10,3 +10,18 @@ print("Hello\nWorld")
 #using quotes
 print("Hello World\nMy Name is " + name + ".")
 print("hello World\n\"My name is " + name + "\".")
+
+#functions
+print(name.lower())
+print(name.upper())
+print(name.upper().isupper())
+print(name.lower().islower())
+print(len(name)) # length of the string
+
+print(name[0]) # print the first letter of the string
+print(name[1]) # prints the second letter of the string, name[2] prints the third etc....
+
+print(name.index("u")) # returns the index where a character is located
+print(name.index("Suli")) # you can also pass as a parameter words
+print(name.replace("Suli", "Surname")) # replases strings
+
