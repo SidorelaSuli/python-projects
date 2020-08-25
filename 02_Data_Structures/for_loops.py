9fb7fbb45c8ddde1901d870c00699397dd5d0fbb
SidorelaSuli <sidorela.suli@gmail.com>
2020-08-25 16:50:55 +0200

    for loops examples

---
+++ 02_Data_Structures/for_loops.py
@@ -0,0 +1,12 @@
+for letter in "sidorela suli":
+    print(letter)
+
+list = ["item1", "item2", "item3"]
+for item in list:
+    print(item)
+
+for index in range(10):
+    print(index)
+
+for i in range(len(list)):
+    print(list[i])
